fff45f722dd0c043e3d7910ae35dd8a6d841f2e0
shilpi bohra <shilpibohra@gmail.com>
2025-05-17 09:48:40 +0530

    ME_BUILDOUT_QTIFY_V2

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,18 +1,36 @@
-//import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-//import Card from "./components/Card/Card";
+import Card from "./components/Card/Card";
 import Hero from "./components/Hero/Hero";
 //@ts-ignore
 import Navbar from "./components/Navbar/Navbar";
-//import { fetchTopAlbums } from "./components/api/api";
-//import Section from "./components/Sections/Section";
+import { fetchTopAlbums } from "./components/api/api";
+import Section from "./components/Section/Section";
 function App() {
-return (
-  <div className="App">
-    <Navbar />
-    <Hero />
+  const [topAlbumData, settopAlbumData] = useState([]);
+  const generateTopAlbumData = async () => {
+    const data = await fetchTopAlbums();
+    console.log(data);
+    settopAlbumData(data);
+  };
+  useEffect(() => {
+    generateTopAlbumData();
+  }, []);
+  console.log(topAlbumData, "topAlbumData");
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero/>
+      <div className="sectionWrapper" >
+      <Section type="album" title="Top Albums" data={topAlbumData} />
+      </div>
+      {/* <div className="cardContainer">
+      {topAlbumData.map((item) => {
+        return <Card key={item.id} data={item} type="album" />;
+      })}
+      </div> */}
     </div>
-);
+  );
 }
 
 export default App;
